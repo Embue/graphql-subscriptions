ed52413bb21f594b90c134f93b96e7776a55abf7
Peter Pavlovich <peter.pavlovich@embue.com>
2023-07-19 19:00:18 -0400

    Add async version of withFilter

--- src/test/tests.ts
+++ src/test/tests.ts
@@ -26,7 +26,7 @@ describe('PubSub', function() {
 
   it('can unsubscribe', async () => {
     const ps = new PubSub();
-    let subId = await ps.subscribe('a', payload => { assert(false); });
+    let subId = await ps.subscribe('a', payload => { /* do nothing */ });
     ps.unsubscribe(subId);
     let succeed = await ps.publish('a', 'test');
     expect(succeed).to.be.undefined;
--- src/with-filter.ts
+++ src/with-filter.ts
@@ -2,6 +2,7 @@ import { $$asyncIterator } from 'iterall';
 
 export type FilterFn<TSource = any, TArgs = any, TContext = any> = (rootValue?: TSource, args?: TArgs, context?: TContext, info?: any) => boolean | Promise<boolean>;
 export type ResolverFn<TSource = any, TArgs = any, TContext = any> = (rootValue?: TSource, args?: TArgs, context?: TContext, info?: any) => AsyncIterator<any>;
+export type AsyncResolverFn<TSource = any, TArgs = any, TContext = any> = (rootValue?: TSource, args?: TArgs, context?: TContext, info?: any) => Promise<AsyncIterator<any>>;
 
 interface IterallAsyncIterator<T> extends AsyncIterator<T> {
   [$$asyncIterator](): IterallAsyncIterator<T>;
@@ -9,12 +10,12 @@ interface IterallAsyncIterator<T> extends AsyncIterator<T> {
 
 export type WithFilter<TSource = any, TArgs = any, TContext = any> = (
   asyncIteratorFn: ResolverFn<TSource, TArgs, TContext>,
-  filterFn: FilterFn<TSource, TArgs, TContext>
+  filterFn: FilterFn<TSource, TArgs, TContext>,
 ) => ResolverFn<TSource, TArgs, TContext>;
 
 export function withFilter<TSource = any, TArgs = any, TContext = any>(
   asyncIteratorFn: ResolverFn<TSource, TArgs, TContext>,
-  filterFn: FilterFn<TSource, TArgs, TContext>
+  filterFn: FilterFn<TSource, TArgs, TContext>,
 ): ResolverFn<TSource, TArgs, TContext> {
   return (rootValue: TSource, args: TArgs, context: TContext, info: any): IterallAsyncIterator<any> => {
     const asyncIterator = asyncIteratorFn(rootValue, args, context, info);
@@ -70,4 +71,64 @@ export function withFilter<TSource = any, TArgs = any, TContext = any>(
 
     return asyncIterator2;
   };
-};
+}
+
+export async function withFilterAsync<TSource = any, TArgs = any, TContext = any>(
+    asyncIteratorFn: AsyncResolverFn<TSource, TArgs, TContext>,
+    filterFn: FilterFn<TSource, TArgs, TContext>,
+): Promise<AsyncResolverFn<TSource, TArgs, TContext>> {
+  return async (rootValue: TSource, args: TArgs, context: TContext, info: any): Promise<IterallAsyncIterator<any>> => {
+    const asyncIterator = await asyncIteratorFn(rootValue, args, context, info);
+
+    const getNextPromise = () => {
+      return new Promise<IteratorResult<any>>((resolve, reject) => {
+
+        const inner = () => {
+          asyncIterator
+              .next()
+              .then(payload => {
+                if (payload.done === true) {
+                  resolve(payload);
+                  return;
+                }
+                Promise.resolve(filterFn(payload.value, args, context, info))
+                    .catch(() => false) // We ignore errors from filter function
+                    .then(filterResult => {
+                      if (filterResult === true) {
+                        resolve(payload);
+                        return;
+                      }
+                      // Skip the current value and wait for the next one
+                      inner();
+                      return;
+                    });
+              })
+              .catch((err) => {
+                reject(err);
+                return;
+              });
+        };
+
+        inner();
+
+      });
+    };
+
+    const asyncIterator2 = {
+      next() {
+        return getNextPromise();
+      },
+      return() {
+        return asyncIterator.return();
+      },
+      throw(error) {
+        return asyncIterator.throw(error);
+      },
+      [$$asyncIterator]() {
+        return this;
+      },
+    };
+
+    return asyncIterator2;
+  };
+}
